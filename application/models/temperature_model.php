6d518acce4222b3308bcd8e87ea14837230e307e
martin-ukf <martin-ukf@users.noreply.github.com>
2018-04-09 19:17:35 +0200

    vytvoreny model Temperatures_model

---
+++ application/models/temperature_model.php
@@ -0,0 +1,52 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 9. 4. 2018
+ * Time: 19:08
+ */
+<?php
+class Temperature_model extends CI_Model {
+
+    public function __construct()
+    {
+
+    }
+    // vrati zoznam teplot
+    function getRows($id= "") {
+        if(!empty($id)){
+            $query = $this->db->get_where('temperatures', array('id' => $id));
+            return $query->row_array();
+        }else{
+            $query = $this->db->get('temperatures');
+            return $query->result_array();
+        }
+    }
+
+    // vlozenie zaznamu
+    public function insert($data = array()) {
+        $insert = $this->db->insert('temperatures', $data);
+        if($insert){
+            return $this->db->insert_id();
+        }else{
+            return false;
+        }
+    }
+
+    // aktualizacia zaznamu
+    public function update($data, $id) {
+        if(!empty($data) && !empty($id)){
+            $update = $this->db->update('temperatures', $data, array('id'=>$id));
+            return $update?true:false;
+        }else{
+            return false;
+        }
+    }
+
+    // odstranenie zaznamu
+    public function delete($id){
+        $delete = $this->db->delete('temperatures',array('id'=>$id));
+        return $delete?true:false;
+    }
+}
+?>
